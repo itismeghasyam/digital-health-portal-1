5d5e558c46083cddd9081551ade86d86604e458b
itismeghasyam <rstudio@ip-10-5-26-136.ec2.internal>
2020-03-04 22:13:08 +0000

    - updated annotations - updated how annotation table is handled

--- projectStudyLevelAnnotations.R
+++ projectStudyLevelAnnotations.R
@@ -47,79 +47,79 @@ synLogin()
 projects.annotations <- list(
   
   ## mPower 1.0 (check anno - Larsson)
-  'syn4993293' = list('Data collection method' = c('active', 'survey'),
-                      'Device type' = c('phone'),
-                      'Sensor type' = c('accelerometer', 'gyroscope', 'magnetometer',
+  'syn4993293' = list('DataCollectionMethod' = c('active', 'survey'),
+                      'DeviceType' = c('phone'),
+                      'SensorType' = c('accelerometer', 'gyroscope', 'magnetometer',
                                         'touchscreen', 'microphone'),
-                      'Device location' = c('pocket', 'hand'),
+                      'DeviceLocation' = c('pocket', 'hand'),
                       'Diagnosis' = c('parkinsons', 'control'),
                       'Assay' = c('tremor', 'walking', 'tap', 'voice')),
   
   ## MoleMapper Public Researcher Portal (check anno - Dan)
-  'syn5576734' = list('Data collection method' = c('active', 'survey'),
-                      'Device type' = c('phone'),
-                      'Sensor type' = c('accelerometer', 'gyroscope', 'magnetometer'),
-                      'Device location' = c('pocket', 'wrist'),
+  'syn5576734' = list('DataCollectionMethod' = c('active', 'survey'),
+                      'DeviceType' = c('phone'),
+                      'SensorType' = c('accelerometer', 'gyroscope', 'magnetometer'),
+                      'DeviceLocation' = c('pocket', 'wrist'),
                       'Diagnosis' = c('control'),
                       'Assay' = c('tremor', 'walking', 'tap', 'voice')),
   
   ## Asthma Health Public Researcher Portal (check anno - Larsson)
-  'syn8361748' = list('Data collection method' = c('survey'),
-                      'Device type' = c('phone'),
-                      'Sensor type' = c('touchscreen'),
-                      'Device location' = c('hand'),
+  'syn8361748' = list('DataCollectionMethod' = c('survey'),
+                      'DeviceType' = c('phone'),
+                      'SensorType' = c('touchscreen'),
+                      'DeviceLocation' = c('hand'),
                       'Diagnosis' = c('asthma', 'control'),
                       'Assay' = c('')),
   
   ## elevateMS (check anno - Abhi)
-  'syn9746170' = list('Data collection method' = c('active', 'passive', 'survey'),
-                      'Device type' = c('phone'),
-                      'Sensor type' = c('accelerometer', 'gyroscope', 'magnetometer',
+  'syn9746170' = list('DataCollectionMethod' = c('active', 'passive', 'survey'),
+                      'DeviceType' = c('phone'),
+                      'SensorType' = c('accelerometer', 'gyroscope', 'magnetometer',
                                         'touchscreen'),
-                      'Device location' = c('pocket', 'hand'),
+                      'DeviceLocation' = c('pocket', 'hand'),
                       'Diagnosis' = c('multiple sclerosis', 'control'),
                       'Assay' = c('tremor', 'walking', 'tap', 'visual/digit symbol substitution task(vsst/dsst)')),
   
   ## Parkinsons Disease Digital Biomarker DREAM Challenge (check anno - Solly)
-  'syn8717496' = list('Data collection method' = c('active', 'survey'),
-                      'Device type' = c('phone'),
-                      'Sensor type' = c('accelerometer', 'gyroscope', 'magnetometer'),
-                      'Device location' = c('pocket', 'hand'),
+  'syn8717496' = list('DataCollectionMethod' = c('active', 'survey'),
+                      'DeviceType' = c('phone'),
+                      'SensorType' = c('accelerometer', 'gyroscope', 'magnetometer'),
+                      'DeviceLocation' = c('pocket', 'hand'),
                       'Diagnosis' = c('parkinsons', 'control'),
                       'Assay' = c('tremor', 'walking', 'dyskinesia','bradykinesia')),
   
   ## CRF Module (check anno - Dan/Megha)
-  'syn10837149' = list('Data collection method' = c('active', 'survey'),
-                       'Device type' = c('phone', 'wearable'),
-                       'Sensor type' = c('accelerometer', 'gyroscope', 'magnetometer',
+  'syn10837149' = list('DataCollectionMethod' = c('active', 'survey'),
+                       'DeviceType' = c('phone', 'wearable'),
+                       'SensorType' = c('accelerometer', 'gyroscope', 'magnetometer',
                                          'camera', 'gps', 'fitbit', 'nonin'),
-                       'Device location' = c('hand'),
+                       'DeviceLocation' = c('hand'),
                        'Diagnosis' = c('control'),
                        'Assay' = c('heartrate', 'walking')),
   
   ## MyHeart Counts Public Researcher Portal (check anno - Larsson)
-  'syn11269541' = list('Data collection method' = c('active', 'survey'),
-                       'Device type' = c('phone'),
-                       'Sensor type' = c('accelerometer', 'gyroscope', 'magnetometer'),
-                       'Device location' = c('pocket', 'wrist'),
+  'syn11269541' = list('DataCollectionMethod' = c('active', 'survey'),
+                       'DeviceType' = c('phone'),
+                       'SensorType' = c('accelerometer', 'gyroscope', 'magnetometer'),
+                       'DeviceLocation' = c('pocket', 'wrist'),
                        'Diagnosis' = c('control'),
                        'Assay' = c('tremor', 'walking', 'tap', 'voice')),
   
   ## PKU Study Project (check anno - Dan/Megha)
-  'syn20486872' = list('Data collection method' = c('active', 'passive', 'survey'),
-                       'Device type' = c('phone', 'wearable'),
-                       'Sensor type' = c('accelerometer', 'gyroscope', 'magnetometer',
+  'syn20486872' = list('DataCollectionMethod' = c('active', 'passive', 'survey'),
+                       'DeviceType' = c('phone', 'wearable'),
+                       'SensorType' = c('accelerometer', 'gyroscope', 'magnetometer',
                                          'fitbit'),
-                       'Device location' = c('pocket', 'wrist'),
+                       'DeviceLocation' = c('pocket', 'wrist'),
                        'Diagnosis' = c('phenylketonuria', 'control'),
                        'Assay' = c('tremor', 'tap', 'sleep', 'heartrate')),
   
   ## MJFF Levodopa Response Study (check anno - solly)
-  'syn20681023' = list('Data collection method' = c('active', 'survey'),
-                       'Device type' = c('wearable'),
-                       'Sensor type' = c('accelerometer', 'gyroscope', 'magnetometer',
+  'syn20681023' = list('DataCollectionMethod' = c('active', 'survey'),
+                       'DeviceType' = c('wearable'),
+                       'SensorType' = c('accelerometer', 'gyroscope', 'magnetometer',
                                          'pebble', 'GeneAcvtiv'),
-                       'Device location' = c('wrist'),
+                       'DeviceLocation' = c('wrist'),
                        'Diagnosis' = c('parkinsons', 'control'),
                        'Assay' = c('tremor', 'bradykinesia', 'dyskinesia')))
 
@@ -166,14 +166,13 @@ for(proj.id in projects.list){
 
 # Merge a list of values for a given key into a string
 for(key in names(annotations.key.value.list)){
-  annotations.key.value.list[[key]] <- paste0(annotations.key.value.list[[key]], collapse = ', ')
+  annotations.key.value.list[[key]] <- as.data.frame(annotations.key.value.list[[key]])
 }
 
 # Make a table out of the previous list
 annotations.key.value.tbl <- annotations.key.value.list %>% 
-  as.data.frame(stringsAsFactors = F) %>% 
-  `colnames<-`(names(annotations.key.value.list)) %>% 
-  tidyr::gather(key = 'annotation key', value = 'list of possible values')
+  data.table::rbindlist(use.names = T, idcol = T) %>% 
+  `colnames<-`(c('key','val'))
 
 # Upload to Synapse
 # Github link
@@ -185,7 +184,7 @@ thisFile <- getPermlink(repository = thisRepo, repositoryPath=thisFileName)
 
 # Write to Synapse
 write.csv(annotations.key.value.tbl,file = paste0('annotation_key_value_pairs','.csv'),na="")
-obj = File(paste0('annotation_key_value_pairs','.csv'), 
-           name = paste0('annotation_key_value_pairs','.csv'), 
+obj = File(paste0('annotation_key_value_pairs','.csv'),
+           name = paste0('annotation_key_value_pairs','.csv'),
            parentId = 'syn21574434')
 obj = synStore(obj, executed = thisFile)
